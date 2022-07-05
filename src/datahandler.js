a1451976f447f81e02845debf431592204f9e6f7
Sami Siddiqui <samisiddiqui@airmail.cc>
2022-07-05 04:16:36 -0500

    linted

--- src/datahandler.js
+++ src/datahandler.js
@@ -1,28 +1,27 @@
 class dataHandler {
-    constructor() {
-        if (dataHandler._instance) {
-            return dataHandler._instance
-        }
-        dataHandler._instance = this;
+  constructor() {
+    if (dataHandler._instance) {
+      return dataHandler._instance;
     }
+    dataHandler._instance = this;
+  }
 
-    getList(list) {
-        return JSON.parse(localStorage.getItem(list));
+  getList(list) {
+    return JSON.parse(localStorage.getItem(list));
+  }
+
+  appendItem(list, value) {
+    const listObjs = this.getList(list);
+    const listObjsArray = [];
+    if (listObjs !== null) {
+      for (let obj = 0; obj < Object.keys(listObjs).length; obj += 1) {
+        const currentObj = listObjs[obj];
+        listObjsArray.push(currentObj);
+      }
     }
-
-    appendItem(list, value) {
-        let listObjs = this.getList(list);
-        let listObjsArray = [];
-        if (listObjs !== null) {
-            for (let obj = 0; obj < Object.keys(listObjs).length; obj++) {
-                let currentObj = listObjs[obj];
-                listObjsArray.push(currentObj);
-            };
-        }
-        listObjsArray.push(value);
-        localStorage.setItem(list, JSON.stringify(listObjsArray));
-
-    }
+    listObjsArray.push(value);
+    localStorage.setItem(list, JSON.stringify(listObjsArray));
+  }
 }
 
 export default dataHandler;
--- src/index.js
+++ src/index.js
@@ -10,58 +10,56 @@ const desc = document.getElementById('desc');
 const button = document.getElementById('submitButton');
 const selectList = document.getElementById('selectList');
 const data = new dataHandler();
-/*const dropdown = document.createElement('select');
+/* const dropdown = document.createElement('select');
 const option = document.createElement('option');
 option.innerHTML = 'default';
 dropdown.appendChild(option);
 const form = document.getElementById('form');
 form.appendChild(dropdown); */
 
-
-
 init();
 
 function init() {
-    //App behavior on first initialization
-    if (localStorage.getItem('activeList') === null) {
-        localStorage.setItem('lists', 'default');
-        localStorage.setItem('activeList', 'default');
+  // App behavior on first initialization
+  if (localStorage.getItem('activeList') === null) {
+    localStorage.setItem('lists', 'default');
+    localStorage.setItem('activeList', 'default');
+  }
+  if ((localStorage.getItem('lists') === 'default')) {
+    const dropdownOption = document.createElement('option');
+    dropdownOption.setAttribute('option', 'default');
+    dropdownOption.innerHTML = 'default';
+    selectList.appendChild(dropdownOption);
+  } else {
+    const lists = data.getList('lists');
+    for (let c = 0; c < Object.keys(lists).length; c++) {
+      const dropdownOption = document.createElement('option');
+      dropdownOption.setAttribute('option', lists[c]);
+      dropdownOption.innerHTML = lists[c];
+      selectList.appendChild(dropdownOption); 1;
     }
-    if ((localStorage.getItem('lists') === 'default')) {
-        let dropdownOption = document.createElement('option');
-        dropdownOption.setAttribute('option', 'default');
-        dropdownOption.innerHTML = 'default';
-        selectList.appendChild(dropdownOption);
-    } else {
-        let lists = data.getList('lists');
-        for (let c = 0; c < Object.keys(lists).length; c++) {
-            let dropdownOption = document.createElement('option');
-            dropdownOption.setAttribute('option', lists[c]);
-            dropdownOption.innerHTML = lists[c];
-            selectList.appendChild(dropdownOption);
-        }
+  }
+  const activeList = (localStorage.getItem('activeList') === null) ? selectList.value : localStorage.getItem('activeList');
+  const activeListJSON = data.getList(activeList);
+  if (activeListJSON !== null) {
+    for (let obj = 0; obj < Object.keys(activeListJSON).length; obj++) {
+      const currentObj = activeListJSON[obj];
+      const newTodo = new todo(currentObj.id, currentObj.priority, currentObj.title, currentObj.description);
+      appendTo('list', 'li', newTodo);
     }
-    let activeList = (localStorage.getItem('activeList') === null) ? selectList.value : localStorage.getItem('activeList');
-    let activeListJSON = data.getList(activeList);
-    if (activeListJSON !== null) {
-        for (let obj = 0; obj < Object.keys(activeListJSON).length; obj++) {
-            let currentObj = activeListJSON[obj];
-            let newTodo = new todo(currentObj.id, currentObj.priority, currentObj.title, currentObj.description);
-            appendTo('list', 'li', newTodo);
-        };
-    }
+  }
 }
 
-button.addEventListener('click', function (e) {
-    //Retrive values for new todo, create and attach to <ul id="list">
-    let newID = (localStorage.getItem('maxID') === null) ? 0 : parseInt(localStorage.getItem('maxID')) + 1;
-    let textValue = text.value;
-    let newPrio = parseInt(prio.value);
-    let setList = selectList.value;
-    let newTodo = new todo(newID, newPrio, textValue, desc.value);
-    appendTo('list', 'li', newTodo);
+button.addEventListener('click', () => {
+  // Retrive values for new todo, create and attach to <ul id="list">
+  const newID = (localStorage.getItem('maxID') === null) ? 0 : parseInt(localStorage.getItem('maxID'), 10) + 1;
+  const textValue = text.value;
+  const newPrio = parseInt(prio.value, 10);
+  const setList = selectList.value;
+  const newTodo = new todo(newID, newPrio, textValue, desc.value);
+  appendTo('list', 'li', newTodo);
 
-    data.appendItem(setList, newTodo);
-    localStorage.setItem('maxID', newID);
-    localStorage.setItem('activeList', selectList.value);
+  data.appendItem(setList, newTodo);
+  localStorage.setItem('maxID', newID);
+  localStorage.setItem('activeList', selectList.value);
 });
